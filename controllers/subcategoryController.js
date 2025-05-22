11336743d8ed647422fd22da2f7f539c704a2960
elbichi <spedeaza@gmail.com>
2025-05-22 17:19:37 -0500

    clase 3 crud

--- controllers/subcategoryController.js
+++ controllers/subcategoryController.js
@@ -28,7 +28,7 @@ exports.createSubcategory = async(req, res)=>{
             data: newSubcategory
         });
     }catch(error){
-        console.error('Error al crear la subcategoria:',err);
+        console.error('Error al crear la subcategoria:',error);
 
 
         if(error.message.includes('duplicate  key')|| error.message.includes('Ya existe')){
@@ -47,3 +47,111 @@ exports.createSubcategory = async(req, res)=>{
 };
 
 //Obtener todas las subcategorias
+exports.getSubcategories = async (req, res)=>{
+    try{
+        const getSubcategories = await Subcategory.findById().pupulate('category', 'name');
+        res.status(200).json({
+            success: true,
+            data: subcategories
+        });
+    } catch (error){
+        console.error('Error al obtener subcategorias',error);
+        res.status(500).json({
+            success: false,
+            message: 'Error al obtener subcategorias'
+        });
+    }
+};
+
+//Obtener subcategorias por id
+exports.getSubcategoryById = async(req, res)=>{
+    try{
+        const Subcategory = await Subcategory.findById
+        (req.params.id).populate('category','name');
+        if(!Subcategory){
+            return res.status(404).json({
+                success: false,
+                message:'Subcategoria no encontrada'
+            });
+        }
+        res.status(200).json({
+            success:true,
+            data: Subcategory
+        });
+    }catch(error){
+        console.error('Error al obtener la subcategoria:', error);
+        res.status(500).json({
+            success: false,
+            message:'Error al obtener la subcategoria'
+        });
+    }
+};
+//Actualizar Subcategoria
+exports.updateSubcategory = async(req, res)=>{
+    try{
+        const{name, description, category} = req.body;
+
+        //Verificar si se cambia la categoria
+        if(category){
+            const parentCategory = await Category.findById(category);
+            if(!parentCategory){
+                return res.status(404).json({
+                    success:false,
+                    message:'La categoria no existe'
+                });
+            }
+        }
+
+        const updateCategory = await Subcategory.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: name? name.trim() : undefined,
+                description: description? description.trim() : undefined,
+                category
+            },
+            {new: true, runValidators: true}
+        );
+
+        if(!updateCategory){
+            return res.status(404).json({
+                success: false,
+                message:'Subcategoria no encontrada'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message:'Subcategoria actualisada',
+            data: updateCategory
+        });
+    }catch(error){
+        console.error('Error al actualizar la subcategoria', error);
+        res.status(500).json({
+            success: false,
+            message:'Error al actualizar la subcategoria'
+        });
+    }
+};
+
+//Elimnar subcategoria
+exports.deleteSubcategory = async(req,res)=>{
+    try{
+        const deletedSubcategory = await Subcategory.findByIdAndDelete(req.params.id);
+        if (!deletedSubcategory){
+            return res.status(404).json({
+                success:false,
+                message:'Subcategoria no encontrada'
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:'Subcategoria eliminada'
+        });
+    }catch(error){
+        console.error('Error al eliminar subcategoria', error);
+        res.status(500).json({
+            success: false,
+            message:'Error al eliminar subcategoria'
+        });
+    }
+};
